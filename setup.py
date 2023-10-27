81c0bf8a88f6e8f92f67d6b9981c05451406577e
OhadRubin <iohadrubin@gmail.com>
2023-10-27 19:39:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,14 @@ setuptools.setup(
     long_description_content_type="text/plain",
     url="",
     packages=["lookup"],
-    include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
-    install_requires=[
+    # install_requires=[
         # By definition, a Custom Component depends on Streamlit.
         # If your component has other Python dependencies, list
         # them here.
         
-    ],
+    # ],
     
     entry_points={"console_scripts": ["lookup = lookup.cli:main"]},
 
