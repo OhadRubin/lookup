e2254eacbf0581bd5087734e4a14a14dbb0f56cf
OhadRubin <iohadrubin@gmail.com>
2023-10-28 08:14:28 +0900

    Update cli.py

--- lookup/cli.py
+++ lookup/cli.py
@@ -1,20 +1,38 @@
 import fire
 from redis import StrictRedis
 from socket import gethostname
+from typing import Optional,Any
+import os
+import sys
+import json
 
-def set(url:str, data:str):
-    hostname = gethostname()
+
+def lset(data:Any, hostname:Optional[str]=None):
+    url = os.environ["REDIS_URL"]
+    if hostname is None:
+        hostname = gethostname()
+    data = json.dumps(data)
     rdict = StrictRedis.from_url(url)
     rdict.set(hostname, data.encode())
 
 
-def get(url:str):
-    hostname = gethostname()
+def lget(hostname:Optional[str]=None):
+    if hostname is None:
+        hostname = gethostname()
     rdict = StrictRedis.from_url(url)
-    return rdict.get(hostname).decode()
+    out = json.loads(rdict.get(hostname).decode())
+    return out 
+
+
+def get_set_argv()
+    if len(sys.argv)==1:
+        sys.argv = lget(url)
+    else:
+        lset(url, sys.argv)
 
 def main():
     fire.Fire()
+
 
 if __name__ == '__main__':
     main()
